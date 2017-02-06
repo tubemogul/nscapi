24a89e060bea5b5807665f4c5c9578078c62dd40
Joseph Herlant <joseph.herlant@tubemogul.com>
2017-02-06 13:24:00 -0800

    Avoid names collision and get rid of one unnecessary global variable

--- main.go
+++ main.go
@@ -9,19 +9,18 @@ import (
 	"time"
 )
 
-var (
-	q   *lfc.Queue
-	cfg struct {
-		apiIP              string
-		apiPort            uint
-		apiCustomFieldRoot string
-		apiTemplatesRoot   string
-		nscaIP             string
-		nscaPort           uint
-		nscaPassword       string
-		nscaEncryption     uint
-	}
-)
+var q *lfc.Queue
+
+type cfg struct {
+	apiIP              string
+	apiPort            uint
+	apiCustomFieldRoot string
+	apiTemplatesRoot   string
+	nscaIP             string
+	nscaPort           uint
+	nscaPassword       string
+	nscaEncryption     uint
+}
 
 // cacheWorker will continuously pull DataPackets out of the queue and update
 // the cache with it. Passing true as parameter will make it loop indefinetly.
@@ -85,16 +84,18 @@ func getUintFromEnv(varName string, defaultValue uint, bitSize int) uint {
 
 // initConfig initializes the configuration based on environment variables and
 // passed flags (in the 12 factor app spirit)
-func initConfig() {
-	flag.StringVar(&cfg.apiIP, "api-ip", getStringFromEnv("NSCAPI_API_IP", "0.0.0.0"), "IP the API should listen on. Default to the NSCAPI_API_IP environment variable. Fallback: 0.0.0.0")
-	flag.UintVar(&cfg.apiPort, "api-port", getUintFromEnv("NSCAPI_API_PORT", 8080, 32), "Port the API should listen on. Default to the NSCAPI_API_PORT environment variable. Fallback: 8080")
-	flag.StringVar(&cfg.apiCustomFieldRoot, "api-custom-fields-root", getStringFromEnv("NSCAPI_API_CUSTOM_FIELDS_ROOT", "custom_fields"), "Root directory the API should use as root of the custom fields hierarchy. Default to the NSCAPI_API_CUSTOM_FIELDS_ROOT environment variable. Fallback: custom_fields")
-	flag.StringVar(&cfg.apiTemplatesRoot, "api-templates-root", getStringFromEnv("NSCAPI_API_TEMPLATES_ROOT", "templates"), "Root directory the API should use to look for its templates (root.tmpl and reports_element.tmpl). Default to the NSCAPI_API_TEMPLATES_ROOT environment variable. Fallback: templates")
-	flag.StringVar(&cfg.nscaIP, "nsca-server-ip", getStringFromEnv("NSCAPI_NSCA_IP", "0.0.0.0"), "IP the NSCA server should listen on. Default to the NSCAPI_NSCA_IP environment variable. Fallback: 0.0.0.0")
-	flag.UintVar(&cfg.nscaPort, "nsca-server-port", getUintFromEnv("NSCAPI_NSCA_PORT", 5667, 16), "Port the NSCA server should listen on. Default to the NSCAPI_NSCA_PORT environment variable. Fallback: 5667")
-	flag.StringVar(&cfg.nscaPassword, "nsca-server-password", getStringFromEnv("NSCAPI_NSCA_PASSWORD", ""), "Password the NSCA server should use. Default to the NSCAPI_NSCA_PASSWORD environment variable. Fallback: ''")
-	flag.UintVar(&cfg.nscaEncryption, "nsca-server-encryption", getUintFromEnv("NSCAPI_NSCA_ENCYPTION", 0, 8), "Number corresponding to the encryption to be used by the NSCA server. Default to the NSCAPI_NSCA_ENCRYPTION environment variable. Fallback: 0. See 'DECRYPTION METHOD' on https://github.com/NagiosEnterprises/nsca/blob/master/sample-config/nsca.cfg.in for more details. Must be <27.")
+func initConfig() *cfg {
+	var conf cfg
+	flag.StringVar(&conf.apiIP, "api-ip", getStringFromEnv("NSCAPI_API_IP", "0.0.0.0"), "IP the API should listen on. Default to the NSCAPI_API_IP environment variable. Fallback: 0.0.0.0")
+	flag.UintVar(&conf.apiPort, "api-port", getUintFromEnv("NSCAPI_API_PORT", 8080, 32), "Port the API should listen on. Default to the NSCAPI_API_PORT environment variable. Fallback: 8080")
+	flag.StringVar(&conf.apiCustomFieldRoot, "api-custom-fields-root", getStringFromEnv("NSCAPI_API_CUSTOM_FIELDS_ROOT", "custom_fields"), "Root directory the API should use as root of the custom fields hierarchy. Default to the NSCAPI_API_CUSTOM_FIELDS_ROOT environment variable. Fallback: custom_fields")
+	flag.StringVar(&conf.apiTemplatesRoot, "api-templates-root", getStringFromEnv("NSCAPI_API_TEMPLATES_ROOT", "templates"), "Root directory the API should use to look for its templates (root.tmpl and reports_element.tmpl). Default to the NSCAPI_API_TEMPLATES_ROOT environment variable. Fallback: templates")
+	flag.StringVar(&conf.nscaIP, "nsca-server-ip", getStringFromEnv("NSCAPI_NSCA_IP", "0.0.0.0"), "IP the NSCA server should listen on. Default to the NSCAPI_NSCA_IP environment variable. Fallback: 0.0.0.0")
+	flag.UintVar(&conf.nscaPort, "nsca-server-port", getUintFromEnv("NSCAPI_NSCA_PORT", 5667, 16), "Port the NSCA server should listen on. Default to the NSCAPI_NSCA_PORT environment variable. Fallback: 5667")
+	flag.StringVar(&conf.nscaPassword, "nsca-server-password", getStringFromEnv("NSCAPI_NSCA_PASSWORD", ""), "Password the NSCA server should use. Default to the NSCAPI_NSCA_PASSWORD environment variable. Fallback: ''")
+	flag.UintVar(&conf.nscaEncryption, "nsca-server-encryption", getUintFromEnv("NSCAPI_NSCA_ENCYPTION", 0, 8), "Number corresponding to the encryption to be used by the NSCA server. Default to the NSCAPI_NSCA_ENCRYPTION environment variable. Fallback: 0. See 'DECRYPTION METHOD' on https://github.com/NagiosEnterprises/nsca/blob/master/sample-config/nsca.cfg.in for more details. Must be <27.")
 	flag.Parse()
+	return &conf
 }
 
 func main() {
@@ -102,16 +103,16 @@ func main() {
 	initCache()
 
 	// Loads config from flags or from env
-	initConfig()
+	srvConf := initConfig()
 
 	// Start the worker that updates the cache
 	go cacheWorker(true)
 
 	// Start the API inside a routine
-	go initAPIServer(cfg.apiIP, cfg.apiPort, cfg.apiCustomFieldRoot, cfg.apiTemplatesRoot)
+	go initAPIServer(srvConf.apiIP, srvConf.apiPort, srvConf.apiCustomFieldRoot, srvConf.apiTemplatesRoot)
 
 	// Start the nsca server
-	cfg := nsca.NewConfig(cfg.nscaIP, uint16(cfg.nscaPort), int(cfg.nscaEncryption), cfg.nscaPassword, queueData)
-	nsca.StartServer(cfg, false)
+	nscaCfg := nsca.NewConfig(srvConf.nscaIP, uint16(srvConf.nscaPort), int(srvConf.nscaEncryption), srvConf.nscaPassword, queueData)
+	nsca.StartServer(nscaCfg, false)
 
 }
