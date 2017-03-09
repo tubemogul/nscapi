90d83bc869c9ec8b91908edfcf51412f2a8fbf63
Joseph Herlant <herlantj@gmail.com>
2017-03-09 09:51:04 -0800

    Quick refactoring of the custom fields

--- custom.go
+++ custom.go
@@ -8,12 +8,14 @@ import (
 	"regexp"
 )
 
-// customFields is a map containing the entire custom fields hierarchy using the
-// following format: customFields[fieldClassifier{hostgroup: hostgroup, service: checkName}][fieldName]=value.
-// checkName can be "all" for the defaults at hostgroup and common level.
-// The content of the common.yaml will use the following format
-// customFields[fieldClassifier{hostgroup: "##common##", service: "all"}][fieldName]=value
-var customFields map[fieldClassifier]map[string]interface{}
+type customFields struct {
+	// customFields contains a map containing the entire custom fields hierarchy using the
+	// following format: fields[fieldClassifier{hostgroup: hostgroup, service: checkName}][fieldName]=value.
+	// checkName can be "all" for the defaults at hostgroup and common level.
+	// The content of the common.yaml will use the following format
+	// fields[fieldClassifier{hostgroup: "##common##", service: "all"}][fieldName]=value
+	fields map[fieldClassifier]map[string]interface{}
+}
 
 // fieldClassifier is used as the key in the customFields
 type fieldClassifier struct {
@@ -22,25 +24,25 @@ type fieldClassifier struct {
 
 // loadCustomFields loads in memory the custom fields based on the yaml
 // hierarchy on disk
-func loadCustomFields(rootPath string) error {
-	customFields = make(map[fieldClassifier]map[string]interface{})
+func (f *customFields) load(rootPath string) error {
+	f.fields = make(map[fieldClassifier]map[string]interface{})
 	if _, err := os.Stat(rootPath); err != nil {
 		return err
 	}
 	// 1st common.yaml. Just ignore the errors for now
-	commonFields, _ := processYamlFile(filepath.Join(rootPath, "common.yaml"))
-	customFields[fieldClassifier{"##common##", "all"}] = make(map[string]interface{})
+	commonFields, _ := f.processYamlFile(filepath.Join(rootPath, "common.yaml"))
+	f.fields[fieldClassifier{"##common##", "all"}] = make(map[string]interface{})
 	if commonFields != nil {
-		customFields[fieldClassifier{"##common##", "all"}] = commonFields
+		f.fields[fieldClassifier{"##common##", "all"}] = commonFields
 	}
 	// Then service/hostgroup/*.yaml. Just ignore the errors for now
 	files, _ := filepath.Glob(filepath.Join(rootPath, "service", "*", "*.yaml"))
-	for _, f := range files {
-		key := fieldClassifier{filepath.Base(filepath.Dir(f)), filepath.Base(f[:len(f)-5])}
-		fields, _ := processYamlFile(f)
-		customFields[key] = make(map[string]interface{})
+	for _, file := range files {
+		key := fieldClassifier{filepath.Base(filepath.Dir(file)), filepath.Base(file[:len(file)-5])}
+		fields, _ := f.processYamlFile(file)
+		f.fields[key] = make(map[string]interface{})
 		if fields != nil {
-			customFields[key] = fields
+			f.fields[key] = fields
 		}
 	}
 	return nil
@@ -48,7 +50,7 @@ func loadCustomFields(rootPath string) error {
 
 // processYamlFile tries to process a yaml file return its content hashed and/or
 // an error
-func processYamlFile(path string) (map[string]interface{}, error) {
+func (f *customFields) processYamlFile(path string) (map[string]interface{}, error) {
 	returnValue := make(map[string]interface{})
 	fc, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -59,24 +61,22 @@ func processYamlFile(path string) (map[string]interface{}, error) {
 	return returnValue, err
 }
 
-// mergeMapFromCustomFields merges the data from a given customFields key (aka
-// yaml file loaded from the disk) into the map given as reference
-func mergeMapFromCustomFields(reference map[string]interface{}, key *fieldClassifier) {
-	if m, ok := customFields[*key]; ok {
-		for f := range m {
-			reference[f] = m[f]
+// lookup gets all the fields available on the given customFields key and merges it with the reference map
+func (f *customFields) lookup(reference map[string]interface{}, key *fieldClassifier) {
+	if m, ok := f.fields[*key]; ok {
+		for field := range m {
+			reference[field] = m[field]
 		}
 	}
 }
 
-// getCustomFields returns a hash containing the custom fields specific for this
-// hostname and checkName
-func getCustomFields(hostname, checkName string) map[string]interface{} {
+// get returns a hash containing the custom fields specific for this hostname and checkName
+func (f *customFields) get(hostname, checkName string) map[string]interface{} {
 	re, _ := regexp.Compile("[^A-Za-z0-9]$")
 	hostgroup := re.ReplaceAllString(hostname, "")
 	resultFields := make(map[string]interface{})
-	mergeMapFromCustomFields(resultFields, &fieldClassifier{"##common##", "all"})
-	mergeMapFromCustomFields(resultFields, &fieldClassifier{hostgroup, "all"})
-	mergeMapFromCustomFields(resultFields, &fieldClassifier{hostgroup, checkName})
+	f.lookup(resultFields, &fieldClassifier{"##common##", "all"})
+	f.lookup(resultFields, &fieldClassifier{hostgroup, "all"})
+	f.lookup(resultFields, &fieldClassifier{hostgroup, checkName})
 	return resultFields
 }
