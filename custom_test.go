db7cc14082eb0d7f83ed2bdfadaa8e4d4cef25b0
Joseph Herlant <joseph.herlant@tubemogul.com>
2017-03-17 13:08:46 -0700

    Add tests for customFields.processYamlFile

--- custom_test.go
+++ custom_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-var lookupUseCase = []struct {
+var lookupUseCases = []struct {
 	custom      *customFields
 	key         *fieldClassifier
 	initialMap  map[string]interface{}
@@ -29,7 +29,7 @@ var lookupUseCase = []struct {
 		initialMap:  map[string]interface{}{"foo": "Bar"},
 		expectedMap: map[string]interface{}{"foo": "NewVal"},
 	},
-	// Lookup for annon existing classifier with data and passing a non empty map
+	// Lookup for a non existing classifier with data and passing a non empty map
 	{
 		custom:      &customFields{fields: map[fieldClassifier]map[string]interface{}{fieldClassifier{hostgroup: "db", service: "serviceB"}: map[string]interface{}{"foo": "Unrelated val"}}},
 		key:         &fieldClassifier{hostgroup: "web", service: "serviceA"},
@@ -53,7 +53,7 @@ var lookupUseCase = []struct {
 }
 
 func TestLookup(t *testing.T) {
-	for _, uc := range lookupUseCase {
+	for _, uc := range lookupUseCases {
 		uc.custom.lookup(uc.initialMap, uc.key)
 		if !reflect.DeepEqual(uc.initialMap, uc.expectedMap) {
 			t.Fatalf("Expecting map to be %v after lookup. Got: %v\n", uc.expectedMap, uc.initialMap)
@@ -61,7 +61,7 @@ func TestLookup(t *testing.T) {
 	}
 }
 
-var getUseCase = []struct {
+var getUseCases = []struct {
 	custom      *customFields
 	hostName    string
 	checkName   string
@@ -113,10 +113,40 @@ var getUseCase = []struct {
 }
 
 func TestGet(t *testing.T) {
-	for _, uc := range getUseCase {
+	for _, uc := range getUseCases {
 		returnedMap := uc.custom.get(uc.hostName, uc.checkName)
 		if !reflect.DeepEqual(returnedMap, uc.expectedMap) {
 			t.Fatalf("Expecting get to return %v. Got: %v\n", uc.expectedMap, returnedMap)
+		}
+	}
+}
+
+var processYamlFileUseCases = []struct {
+	filePath    string
+	expectedMap map[string]interface{}
+	expectedErr string
+}{
+	{filePath: "testData/customFields/common.yaml", expectedMap: map[string]interface{}{"paging": false, "team": []interface{}{"ops"}}, expectedErr: ""},
+	{filePath: "testData/customFields/service/db/all.yaml", expectedMap: map[string]interface{}{"paging": true, "team": []interface{}{"dba", "ops"}, "runbook": "https://wiki.example.org/teams/dba/runbooks.html"}, expectedErr: ""},
+	{filePath: "testData/customFields/service/db/nonExistingFile.yaml", expectedMap: map[string]interface{}{}, expectedErr: "open testData/customFields/service/db/nonExistingFile.yaml: no such file or directory"},
+}
+
+func TestProcessYamlFile(t *testing.T) {
+	for _, uc := range processYamlFileUseCases {
+		custom := &customFields{}
+		returnedMap, err := custom.processYamlFile(uc.filePath)
+		if !reflect.DeepEqual(returnedMap, uc.expectedMap) {
+			t.Fatalf("Expecting processYamlFile of %s to return %v. Got: %v\n", uc.filePath, uc.expectedMap, returnedMap)
+		}
+
+		if err != nil {
+			if err.Error() != uc.expectedErr {
+				t.Fatalf("Expecting processYamlFile of %s to return the error \"%s\". Got: \"%s\"\n", uc.filePath, uc.expectedErr, err)
+			}
+		} else {
+			if uc.expectedErr != "" {
+				t.Fatalf("Expecting processYamlFile of %s to return the error \"%s\". Got: \"%s\"\n", uc.filePath, uc.expectedErr, err)
+			}
 		}
 	}
 }
